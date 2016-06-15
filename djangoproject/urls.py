6bc583bacd2d63537691f97af6f1f743b74cc814
Izabela Holota <iza.holota@gmail.com>
2016-06-15 19:32:03 +0200

    Detials of the photo added

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$','photos.views.home')
+    url(r'^$','photos.views.home'),
+    url(r'^photos/(?P<pk>[0-9]+)$','photos.views.detail')
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,33 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
-from models import Photo
+from .models import Photo
+from .models import PUBLIC
 
 def home(request):
-    photos = Photo.objects.all().order_by('-created_at')
+    """
+    Show home page
+    :param request: HttpRequest
+    :return: HttpResponse
+    """
+    photos = Photo.objects.filter(visibility=PUBLIC).order_by('-created_at')
     context = {"photos_list": photos[:5]}
     return render(request, 'photos/home.html', context)
-
     #return HttpResponse(html)
+
+
+def detail(request, pk):
+    """
+    Shows details page for the photo
+    :param request: HttpRequest
+    :param pk: id of the photo
+    :return: HttpResponse
+    """
+    possible_photos = Photo.objects.filter(pk=pk)
+    photo = possible_photos[0] if len(possible_photos)==1 else None
+    if photo is not None:
+        context = {
+            'photo':photo
+        }
+        return render(request, 'photos/detail.html', context)
+    else:
+        return HttpResponseNotFound('Photo does not exist')

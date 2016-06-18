924e490f0b5cb16407d66560895fe20a1415c05b
Izabela Holota <iza.holota@gmail.com>
2016-06-18 18:34:03 +0200

    Htmls and urls refectoring

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$','photos.views.home'),
-    url(r'^photos/(?P<pk>[0-9]+)$','photos.views.detail')
+    url(r'^$','photos.views.home', name='photos_home'),
+    url(r'^photos/(?P<pk>[0-9]+)$','photos.views.detail', name='photo_detail')
 ]
